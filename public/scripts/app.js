c1fc014f014596d2d725e0d8ba4bcd08b1aa0800
hli30 <harrylll93@gmail.com>
2018-02-27 03:01:08 +0000

    Implemented tweet post and get using ajax

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,3 +4,25 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+$(document).ready(() => {
+  $("#tweetSubmit").on("submit", function(event) {
+    event.preventDefault();
+    let tweet = $(this).serialize();
+    console.log("sent", tweet);
+    $.post({
+      url: "/tweets",
+      data: tweet,
+      success: function(tweet) {
+        console.log("saved tweet");
+      }
+    });
+    $.ajax({
+      url: "/tweets",
+      method: "GET",
+      success: function(tweets) {
+        console.log("getting tweets:", tweets);
+      }
+    });
+  });
+});
+
